ed06220ff74df493f6f3f3057134a985d6448d95
othmanebelkarda <belkardaothmane05@gmail.com>
2024-10-04 18:06:56 +0100

    .

---
+++ index.js
@@ -0,0 +1,30 @@
+const header=document.querySelector('.header');
+if (window.innerWidth >= 768) {
+  header.innerHTML=`
+   <!-- <button class="button" data-text="Awesome">
+        <span class="actual-text">&nbsp;uiverse&nbsp;</span>
+        <span aria-hidden="true" class="hover-text">&nbsp;uiverse&nbsp;</span>
+       </button>-->
+       <div class="container">
+        <div class="header-bar">
+            <h1 class="logo">
+             <button class="button" data-text="Awesome">
+                <span class="actual-text">&nbsp;Isli&nbsp;</span>
+                <span aria-hidden="true" class="hover-text">&nbsp;Isli&nbsp;</span>
+             </button>
+            </h1>
+            <ul class="slider-menu">
+                <li>à propos</li>
+                <li>Nos Centres</li>
+                <li>pré-inscription</li>
+            </ul>
+        </div>
+    </div>
+    <button>hh</button>
+    `
+
+  }else {
+    header.innerHTML=`
+    <h1>hello</h1>
+     `
+  }
