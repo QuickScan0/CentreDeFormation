7afbf4948bb4b45d47b7525c6d63ecf67912c8ed
othmanebelkarda <belkardaothmane05@gmail.com>
2024-10-08 12:12:06 +0100

    V2

--- script/header.js
+++ script/header.js
@@ -1,37 +1,56 @@
 const header=document.querySelector('.header');
 if (window.innerWidth >= 768) {
   header.innerHTML=`
-   <!-- <button class="button" data-text="Awesome">
+   <!--<button class="button" data-text="Awesome">
         <span class="actual-text">&nbsp;uiverse&nbsp;</span>
         <span aria-hidden="true" class="hover-text">&nbsp;uiverse&nbsp;</span>
-       </button>-->
+       </button> -->
        <div class="container">
         <div class="header-bar">
             <h1 class="logo">
-             <button class="button" data-text="Awesome">
-                <span class="actual-text">&nbsp;Isli&nbsp;</span>
-                <span aria-hidden="true" class="hover-text">&nbsp;Isli&nbsp;</span>
-             </button>
+            <img class="logoImg" src="img/isliLogo.png" >
             </h1>
             <ul class="slider-menu">
                 <li>à propos</li>
-                <li>Nos Centres</li>
                 <li>pré-inscription</li>
+                <li>Nos Centres</li>
+                <li>contact</li>
             </ul>
         </div>
     </div>
+  
     `
 
   }else {
     header.innerHTML=`
      <div class="PhoneHeader">
-      <img class="hamburger" src="icons/hamburger.png" >
        <h1 class="logoo">
-        <button class="button" data-text="Awesome">
-         <span class="actual-text">&nbsp;Isli&nbsp;</span>
-         <span aria-hidden="true" class="hover-text">&nbsp;Isli&nbsp;</span>
+       <img class="logooImg" src="img/isliLogo.png" >
         </button>
        </h1>
+<nav role='navigation'>
+		<div id="menuToggle">
+			<input type="checkbox" />
+			<span></span>
+			<span></span>
+			<span></span>
+			<ul id="menu">
+				<a href="#">
+					<li>à propos</li>
+				</a>
+				<a href="#">
+					<li>pré-inscription</li>
+				</a>
+				<a href="#">
+					<li>Nos Centres</li>
+				</a>
+				<a href="#">
+					<li>Contact</li>
+				</a>
+			</ul>
+		</div>
+	</nav>
      </div>
      `
   }
+  
