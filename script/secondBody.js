ec6e556e429144a9a63db89c12c1433f7b1d7778
othmanebelkarda <belkardaothmane05@gmail.com>
2024-10-09 23:18:52 +0100

    addingSecondBody

---
+++ script/secondBody.js
@@ -0,0 +1,36 @@
+document.querySelector('.secondBody').innerHTML=`
+ 
+<div class="card">
+  <p class="heading">
+  Excellence
+  </p>
+  <p>
+  pour refléter le haut niveau de qualité et l’ambition d'aider chaque apprenant à réussir.
+  </p>
+  <p>Chez ISLI
+</p></div>
+
+
+ 
+<div class="card">
+  <p class="heading">
+  Polyvalence
+  </p>
+  <p>
+  pour mettre en avant la diversité des formations proposées (langues, matières académiques, développement personnel, etc.).
+  </p>
+  <p>Chez ISLI
+</p></div>
+
+ 
+<div class="card">
+  <p class="heading">
+  Accompagnement 
+  </p>
+  <p>
+  pour souligner l’attention portée au suivi et au développement individuel de chaque apprenant.
+  </p>
+  <p>Chez ISLI
+</p></div>
+    
+`
