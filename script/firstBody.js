5674f6480c419ee427cf495e1ed86b97326de3b1
othmanebelkarda <belkardaothmane05@gmail.com>
2024-10-13 22:40:40 +0100

    changingFirstBody

--- script/firstBody.js
+++ script/firstBody.js
@@ -1,13 +1,8 @@
-document.querySelector('.firstBody').innerHTML=`
-<div class="firstBodyTittle">
-<p>
-  Rejoignez nos programmes de soutien scolaire 2024-2025
-</p>
-</div>
- <div class="firstBodySubtittle">
-  <p>Bienvenue dans notre Centre de Formation, un espace d'apprentissage conçu pour accompagner les étudiants et les professionnels dans leur développement académique et personnel. Nous offrons des cours de langues (arabe, français, anglais, allemand et espagnol) et un soutien scolaire complet pour les élèves du primaire, du collège et du lycée. Notre centre propose également une préparation spécialisée pour les concours des écoles supérieures, afin de maximiser vos chances de succès. En plus de nos formations académiques, nous proposons des cours de graphisme, de design, et de développement personnel, vous permettant de renforcer vos compétences créatives et de développer des qualités essentielles pour le monde professionnel.
-  <br>
-  Rejoignez-nous pour bâtir votre avenir avec confiance et compétence !</p>
- </div>
- 
-`;
+const firstBody=document.querySelector('.firstBody');
+    firstBody.innerHTML=`
+    <img src="img/Home.png" class="firstBodyImg">
+    
+    </img>
+     
+    `;
+  
