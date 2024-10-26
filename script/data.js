f9b85ab4d708fad9e2b35530a5133505b3900bf2
othmanebelkarda <belkardaothmane05@gmail.com>
2024-10-26 18:08:27 +0100

    modifying footer

--- script/data.js
+++ script/data.js
@@ -37,31 +37,37 @@ export const centreLocation=[
 {
     name:"Qahira",
     num:"0637674544",
+    adress:"N°126 Av. du Caire, Témara 12010",
     lien:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3311.0632914023545!2d-6.925521003210448!3d33.913770600000014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda70d8009f07d15%3A0x6dbdbbf81e41b74d!2sISLI%20Qahira%20-%20Centre%20de%20Formation!5e0!3m2!1sfr!2sma!4v1729264296708!5m2!1sfr!2sma" 
 },
 {
     name:"Scientifique",
     num:"0537413801",
+    adress:"N°88, Avenue Hamza Ibn Abdel Al Moutallib, Av. Hamza Ibn Abdel Al Mouttalib, Témara 12010",
     lien:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3311.2688626227264!2d-6.925435420654327!3d33.90847919999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda70d1080474763%3A0xea4b670c2e55acb7!2sISLI%20Scientifique!5e0!3m2!1sfr!2sma!4v1729263375008!5m2!1sfr!2sma"  
 },
 {
     name:"centre",
     num:"0537405455",
+    adress:"Appart 9, Immeuble 1 Av. Hassan II, Témara 12000",
     lien:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d691.3727227479643!2d-6.918661379843311!3d33.9247092824145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda713e82688b4c1%3A0xef00b75e13211982!2sISLI%20Centre!5e0!3m2!1sfr!2sma!4v1729263418207!5m2!1sfr!2sma" 
 },
 {
-    name:"AbdelKarim_ElKhattabi",
+    name:"AbdelKarim ElKhattabi",
     num:"0537405455",
+    adress:" Av. Abdelkrim El Khattabi, Témara",
     lien:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d827.6544271500677!2d-6.90889910072709!3d33.925237400000015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda7128fceb9af1b%3A0x3c18f9fcd9ac7327!2sIsli!5e0!3m2!1sfr!2sma!4v1729263535257!5m2!1sfr!2sma" 
 },
 {
     name:"Milano",
     num:"0537607050",
+    adress:"Appt 01, Imm 10 rue ben razi, Témara 12000",
     lien:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d827.6496011450508!2d-6.895254000802612!3d33.9257341!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda70dca1e347043%3A0x1caa44e7945bb391!2sISLI%20Milano!5e0!3m2!1sfr!2sma!4v1729263762454!5m2!1sfr!2sma" 
 },
 {
     name:"MoulayRchid",
     num:"0537585478",
+    adress:"N°9, Av. Moulay Rachid, Témara 12010",
     lien:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2265.3095856170066!2d-6.912847662517166!3d33.91945059742683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda713b8661ba221%3A0xb54858683f5c8c8c!2sISLI%20Mly%20Rachid!5e0!3m2!1sfr!2sma!4v1729263988499!5m2!1sfr!2sma" 
 }
 
--- script/footer.js
+++ script/footer.js
@@ -1,15 +1,32 @@
-document.querySelector('.footer').innerHTML=
-`<div class="footerContainer">
-<div class="socialContainer">
-  <img class="iconSocial" src="icons/insta.png">
- <a href="https://www.instagram.com/isli_school?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
-   <p>Instagram</p>
- </a>
+import { centreLocation } from "./data.js"
+let content='';
+centreLocation.forEach((item)=>{
+content+=`
+<div class="footer-column">
+<h3 class="footer-title">Isli ${item.name}</h3>
+<div class="footer-info">
+    <span class="footer-text">Address: ${item.adress})</span>
 </div>
-<div class="socialContainer">
-  <img class="iconSocial" src="icons/face.png">
-  <a href="https://www.facebook.com/isli.temara/?locale=pt_BR">
-   <p>Facebook</p>
-  </a>
- </div>
-</div>`
+<div class="footer-info">
+    <span class="footer-text">Phone: ${item.num}</span>
+</div>
+<div class="footer-info">
+    <span class="footer-text">Email:isli@gmail.com</span>
+</div>
+</div>
+`
+});
+
+document.querySelector('.footer').innerHTML=
+` <div class="footer-container">
+${content}
+</div>
+<div class="social-icons">
+<a href="https://www.instagram.com/your-instagram-profile" target="_blank" rel="noopener noreferrer" aria-label="Follow us on Instagram">
+    <img src="icons/insta.png" alt="Instagram">
+</a>
+<a href="https://www.facebook.com/your-facebook-page" target="_blank" rel="noopener noreferrer" aria-label="Like us on Facebook">
+    <img src="icons/face.png" alt="Facebook">
+</a>
+</div>
+`;
