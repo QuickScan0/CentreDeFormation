8df54525f9c96af74c3f81f0a2b4629491e47689
othmanebelkarda <belkardaothmane05@gmail.com>
2024-11-03 18:28:50 +0100

    adding banner to the home page

--- script/firstBody.js
+++ script/firstBody.js
@@ -1,2 +1,9 @@
-;
+document.querySelector('.firstBody').innerHTML=`
+<div class="hero-banner">
+        <img src="img/Home.png" alt="Programme de soutien scolaire">
+        <div class="overlay">
+            <h2>Rejoignez nos programmes de soutien scolaire 2024-2025:</h2>
+        </div>
+    </div>
+`
   
